90c068a6b044b9551d0cf6f1d2a683a4f26d3314
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2025-07-28 10:24:33 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,36 +1,26 @@
-import express, { Response, Request } from 'express';
+import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import hbs from 'hbs';
 import fs from 'fs';
-
-
 const app = express();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const viewsPath = join(__dirname, '../views');
 const partialsPath = join(__dirname, '../views/partials');
-
 app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
-
 hbs.registerPartials(partialsPath);
-
-
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
     const dataPath = join(__dirname, '../data/data.json');
     fs.readFile(dataPath, 'utf-8', (err, jsonData) => {
         if (err) {
             console.error(err);
-            return res.status(500).send('Error loading data')
+            return res.status(500).send('Error loading data');
         }
-
         const parsedData = JSON.parse(jsonData);
         res.render('index.hbs', parsedData);
-    })
+    });
 });
-
 export default app;
