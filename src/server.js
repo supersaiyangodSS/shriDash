42fab1562b12677c09d1dc4dcabb0db6efb59b91
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-02 19:52:13 +0530

    commit

--- src/server.js
+++
@@ -1,3 +0,0 @@
-import app from './app';
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.{hbs,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
