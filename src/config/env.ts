4aa924597a0632590e47c683aae659a74c56a024
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-07 16:45:12 +0530

    base setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-    PORT: z.coerce.number().default(4000)
+    PORT: z.coerce.number().default(4000),
+    MONGO_URI: z.string().min(1)
 });
 
 export const env = envSchema.parse(process.env);
