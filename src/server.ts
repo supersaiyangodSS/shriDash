50ed0b91aa0a83b0c8905e51df0b6e1b8197387c
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-08 15:19:14 +0530

    server setup

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,39 @@
-import app from './app'
-import { env } from './config/env';
-import { logger } from './utils/logger';
+import "dotenv/config";
+import app from "./app";
+import { env } from "./config/env.config";
+import { logger } from "./utils/logger";
+import { connectDB } from "./config/db.config";
 
-app.listen(env.PORT, () => logger.info(`SERVER IS ON PORT ${env.PORT}`));
+function startServer() {
+  return new Promise((resolve, reject) => {
+    const server = app.listen(env.PORT, resolve);
+    server.on("error", reject);
+  });
+}
+
+async function server() {
+  try {
+    await connectDB();
+    await startServer();
+    logger.info(`Server is on port ${env.PORT}`);
+  } catch (error) {
+    logger.error("Startup Failed: " + error);
+    process.exit(1);
+  }
+}
+
+async function startWithoutDB() {
+  try {
+    await startServer();
+    logger.info("Server running without DB");
+  } catch (error) {
+    logger.error(error);
+    process.exit(1);
+  }
+}
+
+if (env.NO_DB === "true") {
+  startWithoutDB();
+} else {
+  server();
+}
