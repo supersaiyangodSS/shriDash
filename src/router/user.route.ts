bbd21831bbef5103078bc99d08ee19e1ba72cd65
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-08 22:28:25 +0530

    setup users router

--- src/router/user.route.ts
+++ src/router/user.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users.controller";
+import { getUsers, createUser } from "../controllers/users.controller";
+import { validate } from "../middleware/validate.middleware";
+import { createUserSchema } from "../schema/user.schema";
 
 const router = Router();
 
-router.get('/users', getUsers);
+router.get('/', getUsers);
+router.post('/', validate(createUserSchema), createUser);
 
 export default router;
