c75c11cbcd4ccbe7f63e2b121b4b20d91a164934
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-12 22:56:01 +0530

    chores: add env schema

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 const envSchema = z.object({
     PORT: z.coerce.number().default(4000),
     MONGO_URI: z.string().min(1),
-    NO_DB: z.string()
+    NO_DB: z.string(),
+    JWT_SECRET: z.string()
 });
 
 export const env = envSchema.parse(process.env);
