11b439d688f7df1448898bccdf40eee918b3dbe8
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-14 10:50:08 +0530

    feat: add request methods for password compare

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -1,69 +1,83 @@
-import mongoose, { InferSchemaType, Schema, model } from "mongoose";
-import bcrypt from 'bcrypt';
+import { InferSchemaType, Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import { ROLES } from "../../constants/roles";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: {
-         type: String,
-         required: true
+      type: String,
+      trim: true,
+      required: true,
     },
     lastName: {
-         type: String,
-         required: true
+      type: String,
+      trim: true,
+      required: true,
     },
     email: {
-         type: String,
-         unique: true,
-         lowercase: true,
-         trim: true,
-         required: true
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      required: true,
+      index: true,
     },
     username: {
-         type: String,
-         unique: true,
-         trim: true,
-         required: true
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+      index: true,
     },
     password: {
-         type: String,
-         required: true,
-         select: false
+      type: String,
+      required: true,
+      select: false,
     },
     role: {
-         type: String,
-         enum: ['admin', 'superadmin', 'user'],
-         default: 'user'
+      type: String,
+      enum: Object.values(ROLES),
+      default: ROLES.USER,
+      index: true,
     },
     verified: {
-         type: Boolean,
-         default: false
+      type: Boolean,
+      default: false,
     },
     token: {
-         type: String,
-         select: false
+      type: String,
+      select: false,
     },
     isTokenUsed: {
-         type: Boolean,
-         default: false
+      type: Boolean,
+      default: false,
     },
     deleted: {
-          type: Boolean,
-          default: false
+      type: Boolean,
+      default: false,
+      index: true,
     },
     deletedAt: {
-          type: Date,
-          default: null
-    }
-}, {
-     timestamps: true
-});
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 userSchema.pre("save", async function () {
-     if (!this.isModified('password')) return;
+  if (!this.isModified("password")) return;
 
-     const salt = await bcrypt.genSalt(10);
-     this.password = await bcrypt.hash(this.password, salt);
-})
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+     return bcrypt.compare(password, this.password);
+}
 
 export type User = InferSchemaType<typeof userSchema>;
 
-export const User = model('User', userSchema);
+export const User = model("User", userSchema);
