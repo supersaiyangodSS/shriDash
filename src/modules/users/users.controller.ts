cbc52655a5105f55764218e537d70e047f0bf992
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-14 10:48:10 +0530

    chores: file restructure

--- src/modules/users/users.controller.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import { User } from "./user.model";
-import { logger } from "../../utils/logger";
-import crypto from "crypto";
-import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
