a327ca1bad59b38b3c817e043c63bb11ba952e56
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-18 18:05:29 +0530

    feat: add softdelete, getUsers

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,45 +1,66 @@
+import { HTTP_CODES } from "../../constants/httpCodes";
 import { logger } from "../../utils/logger";
 import { User } from "./user.model";
 
 export const createUser = async (data: any) => {
-    const existingUser = await User.findOne({
-        $or: [
-            { email: data.email },
-            { username: data.username }
-        ]
-    }).lean();
+  const existing = await User.findOne({
+    $or: [{ email: data.email }, { username: data.username }],
+  }).lean();
 
-    if (await User.exists({ email: data.email })) {
-        const err: any = new Error('Email already exists');
-        err.statusCode = 409;
-        throw err;
+  if (existing) {
+    if (existing.email === data.email) {
+      const err: any = new Error("Email already exists");
+      err.statusCode = HTTP_CODES.CONFLICT;
+      throw err;
     }
 
-    if (await User.exists({ username: data.username })) {
-        const err: any = new Error('Username already exists');
-        err.statusCode = 409;
-        throw err;
+    if (existing.username === data.username ) {
+      const err: any = new Error("Username already exists");
+      err.statusCode = HTTP_CODES.CONFLICT;
+      throw err;
     }
+  }
 
-    const user = await User.create(data);
-    console.log(data);
-    return user.toObject();
-}
+  const user = await User.create(data);
+  console.log(data);
+  return user.toObject();
+};
 
 export const getUsers = async (page: number, limit: number) => {
-    const skip = (page - 1) * limit;
+  const skip = (page - 1) * limit;
 
-    const users = await User.find({}).skip(skip).limit(limit).lean();
+  const users = await User.find({}).skip(skip).limit(limit).lean();
 
-    const total = await User.countDocuments();
-    const pages = Math.ceil(total/limit);
+  const total = await User.countDocuments();
+  const pages = Math.ceil(total / limit);
 
-    return { users, total , page , pages }
-}
+  return { users, total, page, pages };
+};
 
 export const softDeleteUser = async (id: string) => {
-    return User.findByIdAndUpdate(id , {
-        deleted: true,
-        deletedAt: new Date()
-    }, { new: true })
-}
+  const user = await User.findByIdAndUpdate(
+    id,
+    {
+      deleted: true,
+      deletedAt: new Date(),
+    },
+    { new: true },
+  );
+
+  if (!user) {
+    const err: any = new Error("User not found");
+    err.statusCode = HTTP_CODES.BAD_REQUEST;
+    throw err;
+  }
+  return user;
+};
+
+export const forceDeleteUser = (id: string) => {
+  const user = User.findByIdAndDelete(id);
+  if (!user) {
+    const err: any = new Error("User not found");
+    err.statusCode = HTTP_CODES.BAD_REQUEST;
+    throw err;
+  }
+  return user;
+};
