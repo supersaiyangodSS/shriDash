53273e7f870de76a98c181db0bef47c9457d201a
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-18 18:05:57 +0530

    feat: add delete route for soft and force delete

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import * as controller from './user.controller';
 import { validate } from "../../middleware/validate.middleware";
-import { createUserSchema, updateUserSchema } from "./user.schema";
+import { createUserSchema, updateUserSchema, UserIdSchema } from "./user.schema";
+import { authMiddleware } from "../../middleware/auth.middleware";
+import { allowRoles } from "../../middleware/role.middleware";
+import { ROLES } from "../../constants/roles";
 
 const router = Router();
 
 router.post('/', validate(createUserSchema), controller.createUser);
 router.get('/', controller.getUsers);
-router.delete('/:id', controller.softDeleteUser)
+router.delete('/:id', validate(UserIdSchema, "params"), controller.softDeleteUser)
+router.delete('/:id/force', controller.forceDeleteUser);
 
 export default router;
