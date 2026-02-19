fc14e1e71a6272865459243af89415f46bc34237
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-19 15:05:14 +0530

    feat: add apilimiter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { errorHandler } from './middleware/errorHandler.middleare';
 import dns from 'node:dns';
 import apiRouter from './config/router.config';
+import { apiLimiter } from './config/limiter.config';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(express.json())
 
-app.use('/api', apiRouter);
+app.use('/api', apiLimiter, apiRouter);
 
 app.use(errorHandler);
 
