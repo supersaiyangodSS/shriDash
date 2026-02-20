acfa6d0059e20a70b537c37a2c4530dc666fee98
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-20 11:05:55 +0530

    refactor: fix naming conventions for validator

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import * as controller from './users.controller';
 import { validate } from "../../middleware/validate.middleware";
-import { CreateUserSchema, UpdateUserSchema, UserIdSchema } from "./users.schema";
-import { authMiddleware } from "../../middleware/auth.middleware";
+import { CreateUserValidator, UpdateUserValidator, UserIdValidator } from "./users.validator";
+import { authMiddleware } from "@/middleware/auth.middleware";
 import { allowRoles } from "../../middleware/role.middleware";
 import { ROLES } from "../../constants/roles";
 
 const router = Router();
 
-router.post('/', validate(CreateUserSchema), controller.createUser);
+router.post('/', validate(CreateUserValidator), controller.createUser);
 router.get('/', controller.getUsers);
-router.delete('/:id', validate(UserIdSchema, "params"), controller.softDeleteUser)
+router.delete('/:id', validate(UserIdValidator, "params"), controller.softDeleteUser)
 router.delete('/:id/force', controller.forceDeleteUser);
 router.patch('/:id/restore', controller.restoreDeletedUser);
 
