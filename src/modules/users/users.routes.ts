f10f9a7e7c6e549f37b2e8fc678a868c94d31b5f
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-20 18:26:04 +0530

    refactor: reformatted imports

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import * as controller from '@/modules/users/users.controller';
 import { validate } from "@/middleware/validate.middleware";
-import { CreateUserValidator, UpdateUserValidator, UserIdValidator } from "@/modules/users/users.validator";
+import { CreateUserValidator, UpdateUserValidator, UserIdValidator } from "@/modules/users";
 import { authMiddleware } from "@/middleware/auth.middleware";
 import { allowRoles } from "@/middleware/role.middleware";
 import { ROLES } from "@/constants/roles";
 
 const router = Router();
 
-router.post('/', validate(CreateUserValidator), controller.createUser);
-router.get('/', controller.getUsers);
-router.delete('/:id', validate(UserIdValidator, "params"), controller.softDeleteUser)
-router.delete('/:id/force', controller.forceDeleteUser);
-router.patch('/:id/restore', controller.restoreDeletedUser);
+router.post('/', validate(CreateUserValidator), controller.createUserController);
+router.get('/', controller.getUsersController);
+router.delete('/:id', validate(UserIdValidator, "params"), controller.softDeleteUserController)
+router.delete('/:id/force', controller.forceDeleteUserController);
+router.patch('/:id/restore', controller.restoreDeletedUserController);
 
 export default router;
