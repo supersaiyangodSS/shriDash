0c02b349f4dc54b5840ef6ba2b62afb2769333a6
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-20 11:06:11 +0530

    feat: add custom AppError

---
+++ src/errors/AppError.ts
@@ -0,0 +1,11 @@
+import { logger } from '../utils/logger'
+
+export class AppError extends Error {
+    statusCode: number;
+
+    constructor(message: string, statusCode: number) {
+        logger.warn('APPERROR')
+        super(message)
+        this.statusCode = statusCode
+    }
+}
--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import { logger } from "../utils/logger";
 import { HTTP_CODES } from "../constants/httpCodes";
+import { AppError } from "../errors/AppError";
 
 export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     // logger.info('Error Handler Middleware')
     logger.error(err.message);
-    res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).json({ message: err.message || "Something went wrong" });
+
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    res.status(HTTP_CODES.INTERNAL_SERVER_ERROR).json({ message: "Something went wrong" });
 }
