5519b5fa44efa7a4d8e973977362a2a7527299de
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-20 19:25:58 +0530

    fix: minor fixes with AppError

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,17 +1,23 @@
-import { Request, Response } from "express";
-import { User } from "@/modules/users";
+import { NextFunction, Request, Response } from "express";
+import { User } from "@/modules/users/users.model";
 import { generateTokenService } from "@/modules/auth";
 import { HTTP_CODES } from "@/constants/httpCodes";
+import { AppError } from "@/errors/AppError";
 
-export const loginController = async (req: Request, res: Response) => {
-    const { email } = req.body;
+export const loginController = async (req: Request, res: Response, next: NextFunction) => {
 
-    const user = await User.findOne({ email });
-    if (!user) {
-        return res.status(HTTP_CODES.UNAUTHORIZED).json({ success: false, message: "Invalid credentials" });
+    try {
+        const { email } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new AppError('Invalid credentials', HTTP_CODES.UNAUTHORIZED);
+        }
+
+        const token = generateTokenService({ id: user._id, role: user.role });
+
+        res.json({ token });
+    } catch (error) {
+        next(error)
     }
-
-    const token = generateTokenService({ id: user._id, role: user.role });
-
-    res.json({ token });
 }
