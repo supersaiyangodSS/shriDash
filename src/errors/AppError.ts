0e2562b1e9c5dac389c63e9e8e9e52866b54125b
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-02-27 18:48:00 +0530

    chore: removed logger from AppError middleware

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -4,7 +4,6 @@ export class AppError extends Error {
     statusCode: number;
 
     constructor(message: string, statusCode: number) {
-        logger.warn('APPERROR')
         super(message)
         this.statusCode = statusCode
     }
