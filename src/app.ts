00d7e184f4a962d0df921fe066b149d40ebe1894
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-01 22:49:30 +0530

    chore: add cookie-parser and userencoder

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 import { errorHandler } from '@/middleware/errorHandler.middleware';
 import dns from 'node:dns';
 import apiRouter from '@/router/router';
 import { apiLimiter } from '@/config';
+import cookieParser from 'cookie-parser';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const app = express();
 
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.json())
-
 app.use('/api', apiLimiter, apiRouter);
 
 app.use(errorHandler);
