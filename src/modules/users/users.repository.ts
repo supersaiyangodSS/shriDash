803138922683549e2f33db57fba6be45aa4069e5
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-03 14:12:26 +0530

    feat: findUserByIdRepo, findUserByUsernameRepo, updateUserRepo

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,5 +1,8 @@
+import { AppError } from "@/errors/AppError";
 import { CreatUserDTO } from "./dto/createUser.dto";
 import { User } from "./users.model";
+import { HTTP_CODES } from "@/constants/httpCodes";
+import { UpdateUserDTO } from "./dto/UpdateUser.dto";
 
 export const findByEmailorUsernameRepo = async (
   email: string,
@@ -50,3 +53,31 @@ export const restoreDeleteUserRepo = async (id: string) => {
     { new: true, lean: true },
   );
 };
+
+export const findUserByIdRepo = async (id: string) => {
+  const user = await User.findById(id).lean();
+  if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
+  return user;
+}
+
+export const findUserByUsernameRepo = async (username: string) => {
+  const user = await User.findOne({ username });
+  if(!user) throw new AppError("User not found", HTTP_CODES.CONFLICT);
+  return user;
+}
+
+export const updateUserRepo = async (id: string, data: UpdateUserDTO) => {
+  const user = await User.findById(id);
+
+  if(!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
+
+  if(data.username && data.username !== user.username) {
+    const exists = await User.exists({ username: data.username, _id: { $ne: id } });
+    if (exists) throw new AppError('Username already exists', HTTP_CODES.CONFLICT);
+  }
+
+  Object.assign(user, data);
+
+  await user.save();
+  return user.toObject();
+}
