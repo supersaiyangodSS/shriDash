f961515b6e9562967697d1e2a13436bcc0127715
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-03 14:56:19 +0530

    feat: add login validation to authentication routes

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { loginController } from "@/modules/auth";
 import { validate } from "@/middleware/validate.middleware";
+import { LoginValidator } from "./auth.validator";
 
 const router = Router();
 
-router.post('/login', loginController);
+router.post('/login', validate(LoginValidator), loginController);
 
 export default router;
