9d91628634635e7ff4e75fcac07e2e9fda0f4217
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-04 22:24:16 +0530

    refactor: fix formatting and ensure newline at end of limiter.config.ts

--- src/config/limiter.config.ts
+++ src/config/limiter.config.ts
@@ -6,4 +6,12 @@ export const apiLimiter = rateLimit({
     standardHeaders: 'draft-8',
     legacyHeaders: false,
     ipv6Subnet: 56
-})
+});
+
+export const globalLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 400,
+    standardHeaders: 'draft-8',
+    legacyHeaders: false,
+    ipv6Subnet: 56
+});
