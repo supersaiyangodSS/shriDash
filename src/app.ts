d0db5faccc6465749f145effe4810f9ce105305e
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-04 22:24:00 +0530

    refactor: reorganize middleware usage and update CORS origin

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,31 @@
-import express, { urlencoded, Request, Response } from 'express';
+import express, { urlencoded } from 'express';
 import { errorHandler } from '@/middleware/errorHandler.middleware';
 import dns from 'node:dns';
 import apiRouter from '@/router/router';
-import { apiLimiter } from '@/config';
+import { apiLimiter, globalLimiter } from '@/config';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import cors from 'cors';
 import hpp from 'hpp';
+import compression from 'compression';
+import mongoSanitize from 'express-mongo-sanitize';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const app = express();
 
+app.use(globalLimiter);
 app.use(urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:4001"],
     credentials: true
 }));
+app.use(compression());
 app.use(cookieParser());
-app.use(express.json())
-app.use(apiLimiter);
+app.use(express.json({ limit: '10kb' }));
 app.use(hpp());
-app.use('/api', apiRouter);
+app.use('/api', apiLimiter, apiRouter);
 app.use(errorHandler);
 
 export default app;
