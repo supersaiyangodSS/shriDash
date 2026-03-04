a330621e80c1206b06eda52c52711cbc7beb8342
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-04 22:22:08 +0530

    refactor: enforce strict validation in auth and user validators

--- src/modules/users/users.validator.ts
+++ src/modules/users/users.validator.ts
@@ -6,21 +6,21 @@ export const CreateUserValidator = z.object({
     email: z.string().email('invalid email'),
     username: z.string().min(3, 'username must be at least 3 characters'),
     password: z.string().min(6, 'password must be at least 8 characters'),
-});
+}).strict();
 
 export const UpdateUserValidator = z.object({
     firstName: z.string().min(2).optional(),
     lastName: z.string().min(2).optional(),
     username: z.string().min(3).optional()
-});
+}).strict();
 
 export const UpdateUserPassValidator = z.object({
     password: z.string().min(8, 'Password must be at least 8 characters')
-})
+}).strict();
 
 export const UserIdValidator = z.object({
     id: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid user id")
-});
+}).strict();
 
 export type CreateUserInput = z.infer<typeof CreateUserValidator>;
 export type UpdateUserInput = z.infer<typeof UpdateUserValidator>;
