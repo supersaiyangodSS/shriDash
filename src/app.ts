e7668a965ce876897af67c28c93b0ec5938eee6f
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-04 18:09:02 +0530

    feat: enhance app setup with helmet, cors, and hpp middleware for improved security and request handling

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
-import express, { urlencoded } from 'express';
+import express, { urlencoded, Request, Response } from 'express';
 import { errorHandler } from '@/middleware/errorHandler.middleware';
 import dns from 'node:dns';
 import apiRouter from '@/router/router';
 import { apiLimiter } from '@/config';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import cors from 'cors';
+import hpp from 'hpp';
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const app = express();
 
 app.use(urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json())
+app.use(hpp());
 app.use('/api', apiLimiter, apiRouter);
+
+app.get('/', apiLimiter, (req: Request, res: Response) => {
+    res.json({ message: 'Homepage' });
+})
 
 app.use(errorHandler);
 
