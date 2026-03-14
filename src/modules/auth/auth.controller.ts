a4baa4924dbe34bc1984d3c598819022abb76272
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-14 19:58:33 +0530

    chore: used cookieOptions from config

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,18 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { loginService } from "@/modules/auth";
 import { HTTP_CODES } from "@/constants/httpCodes";
 import { successResponse } from "@/utils/response";
-import { env } from "@/config";
+import { env, cookieOptions } from "@/config";
 
 export const loginController = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = await loginService(req.body);
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: env.NODE_ENV === "production",
-            sameSite: "strict",
+            ...cookieOptions,
             maxAge: 24 * 60 * 60 * 1000,
         });
-        successResponse(res, HTTP_CODES.OK, 'logged in successful');
+        successResponse(res, HTTP_CODES.OK, 'login successful');
     } catch (error) {
         next(error)
     }
@@ -26,7 +24,7 @@ export const logoutController = async (req: Request, res: Response, next: NextFu
             secure: env.NODE_ENV === "production",
             sameSite: "strict",
         });
-        successResponse(res, HTTP_CODES.OK, 'logged out successful');
+        successResponse(res, HTTP_CODES.OK, 'logout successful');
     } catch (error) {
         next(error);
     }
