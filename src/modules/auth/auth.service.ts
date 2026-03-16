4ac25a80a4f5b84dfd273120ee48033342d1ab20
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-16 21:38:08 +0530

    refactor: add DB login into services

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,20 +1,30 @@
 import jwt from 'jsonwebtoken';
 import { env } from '@/config';
 import { LoginDto } from './auth.validator';
-import { authenticateUserRepo } from './auth.repository';
 import { AppError } from '@/errors/AppError';
 import { HTTP_CODES } from '@/constants/httpCodes';
 import { AuthUser } from './types/auth.types';
+import { User } from '../users/users.model';
 
 export const generateTokenService = (payload: AuthUser) => {
     return jwt.sign(payload, env.JWT_SECRET, { expiresIn: "7d" });
 }
 
 export const loginService = async (payload: LoginDto) => {
-    const user = await authenticateUserRepo(payload.email, payload.password);
-    if (!user) throw new AppError('Invalid credentials', HTTP_CODES.UNAUTHORIZED);
+    const user = await User.findOne({ email: payload.email, deleted: false }).select('+password');
 
-    const token = generateTokenService(user);
+    if (!user){
+         throw new AppError('Invalid credentials', HTTP_CODES.UNAUTHORIZED);
+        }
+
+    const isPasswordValid = await user.comparePassword(payload.password);
+
+    if (!isPasswordValid) {
+         throw new AppError("Invalid credentials", HTTP_CODES.UNAUTHORIZED);
+        }
+    const { _id, role, } = user;
+
+    const token = generateTokenService({ id: _id.toString(), role });
 
     return token;
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,17 @@
 import { AppError } from "@/errors/AppError";
 import { HTTP_CODES } from "@/constants/httpCodes";
-import * as userRepository from "@/modules/users/users.repository";
 import { sendEmail } from "@/utils/nodemailer";
 import { generateVerificationToken } from "@/utils/generateToken";
 import { CreateUserDto, UpdateUserDto } from "./users.validator";
+import { User } from "./users.model";
 
 export const createUser = async (data: CreateUserDto) => {
-  const existing = await userRepository.findByEmailorUsernameRepo(
-    data.email,
-    data.username,
-  );
+  const existing = await User.findOne({
+    $or: [
+      { email: data.email },
+      { username: data.username }
+    ]
+  })
   if (existing) {
     if (existing.email === data.email) {
       throw new AppError("Email already exists", HTTP_CODES.CONFLICT);
@@ -19,76 +21,135 @@ export const createUser = async (data: CreateUserDto) => {
 
   const token = generateVerificationToken();
   const userData = { ...data, token };
-  const user = await userRepository.createUserRepo(userData);
+  const user = await User.create(userData);
+
   const verifyUrl = `http://localhost:4001/api/user/verify-email/${token}`;
 
   await sendEmail(data.email, 'Verify Email', `<a href="${verifyUrl}">Verify your email</a>`);
-  return user;
+  const { password, token: _, ...safeUser } = user.toObject();
+  return safeUser;
 };
 
 export const getUsers = async (page: number, limit: number) => {
-  const skip = (page - 1) * limit;
-  const users = await userRepository.getUsersRepo(limit, skip);
-  const total = await userRepository.countUsersRepo();
-  const pages = Math.ceil(total / limit);
-  return { users, total, page, pages };
+  const safePage = Math.max(page, 1);
+  const safeLimit = Math.max(limit, 1);
+  const skip = (safePage - 1) * safeLimit;
+
+  const users = await User.find()
+    .skip(skip).limit(safeLimit).lean();
+
+  const total = await User.countDocuments();
+  const pages = Math.ceil(total / safeLimit);
+  return { users, total, page: safePage, pages };
 };
 
 export const softDeleteUser = async (id: string) => {
-  const user = await userRepository.findByIdRepo(id);
-  if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
-  const deletedUser = await userRepository.checkSoftDeletedUserRepo(id);
-  if (deletedUser) throw new AppError("User is already deleted", HTTP_CODES.CONFLICT);
-  return await userRepository.softDeleteUserRepo(id);
+   const user = await User.findOneAndUpdate(
+   { _id: id, deleted: false },
+    {
+      $set: {
+        deleted: true,
+        deletedAt: new Date(),
+      },
+    },
+    { new: true },
+  ).lean();
+  if (!user) throw new AppError("User not found or already deleted" , HTTP_CODES.NOT_FOUND);
+  return user;
 };
 
 export const forceDeleteUser = async (id: string) => {
-  const user = await userRepository.findByIdRepo(id);
+  const user = await User.findByIdAndDelete(id).lean();
   if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
-  return await userRepository.forceDeleteUserRepo(id);
+  return user;
 };
 
 export const restoreDeletedUser = async (id: string) => {
-  const user = await userRepository.findByIdRepo(id);
-  if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
-  const deletedUser = await userRepository.checkSoftDeletedUserRepo(id);
-  if (!deletedUser) throw new AppError('User is not deleted', HTTP_CODES.NOT_FOUND);
-  return await userRepository.restoreDeleteUserRepo(id);
+  const user = await User.findOneAndUpdate(
+    { _id: id, deleted: true },
+    { $set: { deleted: false }, $unset: { deletedAt: "" } },
+    { new: true }
+  ).lean();
+  if (!user) throw new AppError("User not found or not deleted", HTTP_CODES.NOT_FOUND);
+  return user;
 };
 
 export const updateUser = async (id: string, payload: UpdateUserDto) => {
-  const user = await userRepository.findByIdRepo(id);
-  if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
-  const updatedUser = await userRepository.updateUserRepo(id, payload);
-  return updatedUser;
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
+  }
+
+  if (payload.username) {
+    const normlizedUsername = payload.username.trim().toLowerCase();
+
+    if (user.username === normlizedUsername) {
+      throw new AppError("Username must be different", HTTP_CODES.CONFLICT);
+    }
+
+    const exists = await User.exists({
+      username: normlizedUsername,
+      _id: { $ne: id }
+    });
+
+    if (exists) {
+      throw new AppError("Username already exists", HTTP_CODES.CONFLICT);
+    }
+    user.username = normlizedUsername;
+  }
+
+  const { username, ...rest } = payload;
+  Object.assign(user, rest);
+  await user.save()
+  return user.toObject({ versionKey: false });
+  // const { password, ...safeUser } = user.toObject();
+  // return safeUser;
 }
 
 export const updateUserPass = async (id: string, oldPassword: string, newPassword: string) => {
-  const user = await userRepository.updateUserPassRepo(id, oldPassword, newPassword);
+  const user = await User.findById(id).select("+password");
+
   if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
-  return user;
+
+  const isMatch = await user.comparePassword(oldPassword);
+
+  if (!isMatch) throw new AppError("Invalid current password", HTTP_CODES.BAD_REQUEST);
+
+  const isSame = await user.comparePassword(newPassword);
+  if (isSame) throw new AppError("New password cannot be same as old password", HTTP_CODES.BAD_REQUEST);
+  user.password = newPassword;
+
+  await user.save();
+  const userObj = user.toObject();
+  const { password, ...safeUser } = userObj;
+  return safeUser;
 }
 
 export const updateUserEmail = async (id: string, email: string) => {
-  const normalizedEmail = email
-  const user = await userRepository.findByIdRepo(id);
+  const normalizedEmail = email;
+  const user = await User.findById(id);
   if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
 
   if(user.email === normalizedEmail) {
     throw new AppError("Email must be different from current email", HTTP_CODES.CONFLICT);
   }
-  const existingUser = await userRepository.emailExists(normalizedEmail, id);
+  const existingUser = await User.exists({ email: normalizedEmail, _id: { $ne: id } });
   if (existingUser) throw new AppError("Email already exists", HTTP_CODES.CONFLICT);
 
-  const updatedUser = await userRepository.updateUserEmailRepo(id, normalizedEmail);
-  return updatedUser;
+  user.email = normalizedEmail;
+  await user.save();
+  return user;
 }
 
 export const verifyEmail = async (token: string) => {
-  const user = await userRepository.findByTokenRepo(token);
+  const user = await User.findOne({token}).select("+token");
   if (!user) throw new AppError("Invalid token", HTTP_CODES.BAD_REQUEST);
 
-  user.verified = true,
-  user.token = undefined,
-  user.isTokenUsed = true
+  user.verified = true;
+  user.token = undefined;
+  user.isTokenUsed = true;
+  await user.save();
+  return user.toObject({ versionKey: false })
 }
