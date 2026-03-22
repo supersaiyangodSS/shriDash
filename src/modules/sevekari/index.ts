887e149ab3fb679f975352a16bc73d1b9cf3a834
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-22 22:52:17 +0530

    feat: add barrel exports for sevekari

---
+++ src/modules/sevekari/index.ts
@@ -0,0 +1,5 @@
+export * from "@/modules/sevekari/sevekari.controller";
+export * from "@/modules/sevekari/sevekari.routes";
+export * from "@/modules/sevekari/sevekari.service";
+export * from "@/modules/sevekari/sevekari.validator";
+export * from "@/modules/sevekari/sevekari.types";
