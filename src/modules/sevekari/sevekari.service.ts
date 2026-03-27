ae7c40114d9ca99df367c7c389d91896e74197fe
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-27 23:05:14 +0530

    feat: add soft and force delete sevekari

--- src/modules/sevekari/sevekari.service.ts
+++ src/modules/sevekari/sevekari.service.ts
@@ -1,8 +1,10 @@
+import "@/modules/temple/temple.model";
 import { HTTP_CODES } from "@/constants/httpCodes";
 import { Sevekari } from "./sevekari.model";
 import { SevekariDto } from "./sevekari.validator";
 import { MESSAGE } from "@/constants/messages";
 import { AppError } from "@/errors/AppError";
+import { logger } from "@/utils/logger";
 
 export const createSevekari = async (payload: SevekariDto) => {
   const existing = await Sevekari.exists({
@@ -21,5 +23,32 @@ export const createSevekari = async (payload: SevekariDto) => {
 };
 
 export const getSevekari = async () => {
-  return await Sevekari.find().populate("Temple");
+  const data = await Sevekari.find({ deleted: false }).populate("templeId");
+  data.forEach((item) => {
+    if (!item.templeId) {
+      logger.warn("Missing temple id for sevekari");
+    }
+  });
+  return data;
+};
+
+export const softDeleteSevekari = async (id: string) => {
+  const user = await Sevekari.findOneAndUpdate(
+    { _id: id, deleted: false },
+    {
+      $set: {
+        deleted: true,
+      },
+    },
+    { new: true, projection: { __v: 0 } },
+  ).lean();
+  if (!user)
+    throw new AppError("User not found or deleted", HTTP_CODES.NOT_FOUND);
+  return user;
+};
+
+export const forceDeleteSevekari = async (id: string) => {
+  const user = await Sevekari.findByIdAndDelete({ _id: id }).lean();
+  if (!user) throw new AppError("User not found", HTTP_CODES.NOT_FOUND);
+  return user;
 };
