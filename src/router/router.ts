c467642bb3f046f07ee6a90d2d3a07fa492d43ad
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-03-31 23:14:48 +0530

    chore: route path and import path changed for audit logs route

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import userRouter from "@/modules/users/users.routes";
 import authRouter from "@/modules/auth/auth.routes";
 import sevekariRouter from "@/modules/sevekari/sevekari.routes";
-import auditRouter from "@/modules/admin/audit/audit.routes";
+import auditRouter from "@/modules/audit/audit.routes";
 
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
 router.use("/sevekari", sevekariRouter);
-router.use("/admin", auditRouter);
+router.use("/audit", auditRouter);
 
 export default router;
