32341e7d0384093df9be6fba9bdc590e05fcc2f8
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-04-07 22:48:47 +0530

    feat: add audit model

---
+++ src/modules/audit/audit.model.ts
@@ -0,0 +1,46 @@
+import { Schema, Types, model } from "mongoose";
+
+const auditLogSchema = new Schema(
+  {
+    userId: {
+      type: Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    role: {
+      type: String,
+      default: "Guest",
+    },
+    action: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    resource: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    resourceId: {
+      type: Types.ObjectId,
+      default: null,
+    },
+    method: {
+      type: String,
+      trim: true,
+    },
+    url: {
+      type: String,
+      trim: true,
+    },
+    ip: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AuditLog = model("AuditLog", auditLogSchema);
