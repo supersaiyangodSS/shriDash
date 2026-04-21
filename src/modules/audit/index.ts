dfaf7b5a37a18aef14c9bdc91b818666e1423d82
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-04-21 20:52:55 +0530

    chore: add audit.routes to audit index file

--- src/modules/audit/index.ts
+++ src/modules/audit/index.ts
@@ -1,4 +1,4 @@
 export * from "@/modules/audit/audit.controller";
 export * from "@/modules/audit/audit.model";
-export * from "@/modules/audit/audit.routes";
 export * from "@/modules/audit/audit.service";
+export * from "@/modules/audit/audit.routes";
