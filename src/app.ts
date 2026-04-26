ca1d79adfb993ab8fdd9d4cdfd180eb119815196
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-04-26 22:09:12 +0530

    feature: add tailwindcss with script and serve public as static

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,7 @@ app.use(compression());
 app.use(cookieParser());
 app.use(express.json({ limit: "10kb" }));
 app.use(hpp());
+app.use(express.static("public"));
 
 if (env.NODE_ENV !== "production") {
   app.use(
