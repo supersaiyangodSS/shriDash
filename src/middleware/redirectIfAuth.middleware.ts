d463020825b23dd1ebdad1a37e6733075a8a42ef
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-04-29 19:26:46 +0530

    feat: add redirect middleware for login form and add WebAuthMiddleware for web routes

---
+++ src/middleware/redirectIfAuth.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "@/config";
+
+export const redirectIfAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) return next();
+    jwt.verify(token, env.JWT_SECRET);
+    return res.redirect("/");
+  } catch (error) {
+    res.clearCookie("token");
+    return next();
+  }
+};
---
+++ src/middleware/webAuth.middleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "@/config/env.config";
+
+export const WebAuthMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const cookieToken = req.cookies?.token;
+    const header = req.get("authorization");
+    const headerToken = header?.startsWith("Bearer ")
+      ? header.split(" ")[1]
+      : null;
+
+    const token = cookieToken || headerToken;
+    if (!token) {
+      return res.redirect("/login");
+    }
+    const decoded = jwt.verify(token, env.JWT_SECRET);
+    (req as any).user = decoded;
+    return next();
+  } catch (error) {
+    res.clearCookie("token");
+    return res.redirect("/login");
+  }
+};
