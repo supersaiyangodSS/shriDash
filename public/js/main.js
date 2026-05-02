fe999a9a39d089aefcb6a3dd444668e3d6a0c554
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-05-02 20:22:09 +0530

    feat: add password show/hide button to login form and signup form, add signup form validator logic

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 // NOTYF
 
 const notyf = new Notyf();
@@ -15,33 +16,136 @@ const loginEmail = document.querySelector("#loginFieldEmail");
 const loginPassword = document.querySelector("#loginFieldPassword");
 const loginBtnSubmit = document.querySelector("#loginBtnSubmit");
 
-loginBtnSubmit.addEventListener("click", (e) => {
-  const email = loginEmail.value.trim();
-  const password = loginPassword.value.trim();
+const loginForm = document.querySelector("#loginForm");
 
-  if (!email) {
+if (loginForm) {
+  loginBtnSubmit.addEventListener("click", (e) => {
+    const email = loginEmail.value.trim();
+    const password = loginPassword.value.trim();
+
+    if (!email) {
+      e.preventDefault();
+      notyf.error("Email is required");
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      e.preventDefault();
+      notyf.error("Invalid email");
+      return;
+    }
+
+    if (!password) {
+      e.preventDefault();
+      notyf.error("Password is required");
+      return;
+    }
+
+    if (password.length < 8) {
+      e.preventDefault();
+      notyf.error("Password must be at least 8 characters");
+      return;
+    }
+  });
+}
+
+const signUpForm = document.querySelector("#signupform");
+const firstNameSignup = document.querySelector("#signUpFieldFirstName");
+const lastNameSignup = document.querySelector("#signUpFieldLastName");
+const usernameSignup = document.querySelector("#signUpFieldUsername");
+const emailSignup = document.querySelector("#signUpFieldEmail");
+const passwordSignup = document.querySelector("#signUpFieldPassword");
+const confirmPasswordSignup = document.querySelector(
+  "#signUpFieldConfirmPassword",
+);
+const signUpBtnSubmit = document.querySelector("#signUpBtnSubmit");
+
+if (signUpForm) {
+  signUpBtnSubmit.addEventListener("click", (e) => {
+    const firstName = firstNameSignup.value.trim();
+    const lastName = lastNameSignup.value.trim();
+    const username = usernameSignup.value.trim();
+    const email = emailSignup.value.trim();
+    const password = passwordSignup.value.trim();
+    const confirmPassword = confirmPasswordSignup.value.trim();
+
+    if (!firstName || firstName.length < 3) {
+      e.preventDefault();
+      notyf.error("Invalid first name");
+      return;
+    }
+
+    if (!lastName || lastName.length < 3) {
+      e.preventDefault();
+      notyf.error("Invalid last name");
+      return;
+    }
+
+    if (!username || lastName.length < 3) {
+      e.preventDefault();
+      notyf.error("Invalid username");
+      return;
+    }
+
+    if (!email) {
+      e.preventDefault();
+      notyf.error("Invalid email");
+      return;
+    }
+
+    if (!emailRegex.test(email)) {
+      e.preventDefault();
+      notyf.error("Invalid email");
+      return;
+    }
+
+    if (!password || password.length < 8) {
+      e.preventDefault();
+      notyf.error("Invalid password");
+      return;
+    }
+
+    if (!confirmPassword || confirmPassword !== password) {
+      e.preventDefault();
+      notyf.error("Password does not match");
+      return;
+    }
+  });
+}
+
+const showLoginPasswordBtn = document.querySelector("#showPasswordBtn");
+
+if (showLoginPasswordBtn) {
+  showLoginPasswordBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    notyf.error("Email is required");
-    return;
-  }
+    const passwordInput = document.querySelector("#loginFieldPassword");
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      showLoginPasswordBtn.innerText = "hide";
+    } else {
+      passwordInput.type = "password";
+      showLoginPasswordBtn.innerText = "show";
+    }
+  });
+}
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+const showSignupPasswordBtn = document.querySelector("#showSignupPasswordBtn");
 
-  if (!emailRegex.test(email)) {
+if (showSignupPasswordBtn) {
+  showSignupPasswordBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    notyf.error("Invalid email");
-    return;
-  }
-
-  if (!password) {
-    e.preventDefault();
-    notyf.error("Password is required");
-    return;
-  }
-
-  if (password.length < 8) {
-    e.preventDefault();
-    notyf.error("Password must be at least 8 characters");
-    return;
-  }
-});
+    const passwordInput = document.querySelector("#signUpFieldPassword");
+    const confirmPasswordInput = document.querySelector(
+      "#signUpFieldConfirmPassword",
+    );
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      confirmPasswordInput.type = "text";
+      showSignupPasswordBtn.innerText = "hide";
+    } else {
+      passwordInput.type = "password";
+      confirmPasswordInput.type = "password";
+      showSignupPasswordBtn.innerText = "show";
+    }
+  });
+}
