f10a6ead41af7b40d518435d80a330401b0731ba
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-05-06 23:49:06 +0530

    feat: add renderHTMX pages function

---
+++ src/utils/renderPage.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+
+type RenderOptions = Record<string, any>;
+
+export function renderHTMX(
+  req: Request,
+  res: Response,
+  view: string,
+  options: RenderOptions = {},
+  layout: string = "dashboard",
+) {
+  const isHTMX = req.headers["hx-request"];
+  return res.render(view, {
+    ...options,
+    layout: isHTMX ? false : layout,
+  });
+}
