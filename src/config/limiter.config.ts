968fd663e55b3f4e1548df470e271a027aa251f8
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2026-05-10 23:35:54 +0530

    fix: updated api and global limiter to fix exceeding limit issues for frontend and the API

--- src/config/limiter.config.ts
+++ src/config/limiter.config.ts
@@ -2,15 +2,14 @@ import { rateLimit } from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 100,
+  limit: 400,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   ipv6Subnet: 56,
 });
-
 export const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  limit: 400,
+  limit: 500,
   standardHeaders: "draft-8",
   legacyHeaders: false,
   ipv6Subnet: 56,
